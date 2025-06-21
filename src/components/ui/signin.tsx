26adbb90af01e31f27d18426dc0a4726f86fc055
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-06-22 04:31:09 +0530

    update layout and remove placeholder text

--- src/components/ui/signin.tsx
+++ src/components/ui/signin.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
+import { IoChevronBackSharp } from "react-icons/io5";
 
 const SignInPPage = () => {
   const [number, setNumber] = useState(0);
 
   return (
     <>
-      <h1>hello</h1>
+      <div className="flex h-screen items-center justify-center">
+        <div className="flex flex-col gap-4">
+          <Link href="/" className="flex items-center gap-2">
+            <IoChevronBackSharp className="h-10 w-10" />
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
